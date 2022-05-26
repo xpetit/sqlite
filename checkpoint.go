f285f1ec9b406f0ec8218cab6c74bf0d945a3731
xpetit <32063953+xpetit@users.noreply.github.com>
2022-05-26 14:23:33 +0200

    Remove interface to simplify things

--- checkpoint.go
+++ checkpoint.go
@@ -6,25 +6,8 @@ import (
 	"sync"
 )
 
-// Checkpointer is an SQLite WAL checkpointer, it is workaround before WAL2 becomes common:
-// https://www.sqlite.org/cgi/src/doc/wal2/doc/wal2.md
-// Currently, concurrent writes in SQLite make the WAL file grow without limit
-type Checkpointer interface {
-	// Checkpoint checks if the number of times it has been called reaches the limit
-	// If it did, it blocks until all other functions that call it are finished and performs a checkpoint
-	// It is intended to be used like this:
-	//
-	// 	var c = sqlite.NewCheckPointer(db, 1000)
-	// 	func() {
-	// 		defer c.Checkpoint()()
-	// 		db.Exec(`insert into "table" values ("value")`)
-	// 	}
-	Checkpoint() func()
-}
-
-var _ Checkpointer = (*checkpointer)(nil)
-
-type checkpointer struct {
+// Checkpointer is an opaque structure, see NewCheckPointer
+type Checkpointer struct {
 	m     sync.Mutex
 	wg    sync.WaitGroup
 	db    *sql.DB
@@ -32,18 +15,30 @@ type checkpointer struct {
 	i     uint
 }
 
-func NewCheckPointer(db *sql.DB, limit uint) (Checkpointer, error) {
+// NewCheckPointer returns an SQLite WAL checkpointer, it is a workaround before WAL2 becomes common:
+// https://www.sqlite.org/cgi/src/doc/wal2/doc/wal2.md
+// Currently, concurrent writes in SQLite make the WAL file grow without limit
+func NewCheckPointer(db *sql.DB, limit uint) (*Checkpointer, error) {
 	if _, err := db.Exec(`pragma wal_autocheckpoint = 0`); err != nil {
 		return nil, err
 	}
 
-	return &checkpointer{
+	return &Checkpointer{
 		db:    db,
 		limit: limit,
 	}, nil
 }
 
-func (c *checkpointer) Checkpoint() func() {
+// Checkpoint checks if the number of times it has been called reaches the limit
+// If it did, it blocks until all other functions that call it are finished and performs a checkpoint
+// It is intended to be used like this:
+//
+// 	var c = sqlite.NewCheckPointer(db, 1000)
+// 	func() {
+// 		defer c.Checkpoint()()
+// 		db.Exec(`insert into "table" values ("value")`)
+// 	}
+func (c *Checkpointer) Checkpoint() func() {
 	c.m.Lock()
 	if c.i < c.limit {
 		c.i++
